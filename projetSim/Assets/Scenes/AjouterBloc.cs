30b31dc28dbf237342fdd3bc9d4835cd206baa7e
FAntoineLefebvre <123586434+FAntoineLefebvre@users.noreply.github.com>
2023-03-08 15:09:09 -0500

    Changement du code

--- projetSim/Assets/Scenes/AjouterBloc.cs
+++ projetSim/Assets/Scenes/AjouterBloc.cs
@@ -1,29 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
-//using UnityEngine.UIElements;
 
 public class AjouterBloc : MonoBehaviour
 {
     public Canvas canvas;
     public RectTransform rTransform;
     public TMP_Dropdown dropdown;
-    
+
     public void Ajouter()
     {
         var positionInitiale = rTransform.localPosition;
         var positionFinale = new Vector3(rTransform.localPosition.x, rTransform.localPosition.y - 40, rTransform.localPosition.z);
         rTransform.localPosition = positionFinale;
 
-        dropdown.transform.SetParent(canvas.transform, false);
-        dropdown.transform.localPosition = positionInitiale;
-        
-
-      
-
+        var drop = Instantiate(dropdown);
+        drop.transform.SetParent(canvas.transform, false);
+        drop.transform.localPosition = positionInitiale;
+        BlocBouger blocBouger = new BlocBouger(drop);
     }
+    
 }
 
--- projetSim/Assets/Scenes/BlocBouger.cs
+++ projetSim/Assets/Scenes/BlocBouger.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BlocBouger : Bloc
 {
+
     public Direction direction = Direction.AVANT;
+    public TMP_Dropdown dropdown;
+
+    public BlocBouger(TMP_Dropdown dropdown)
+    {
+        this.dropdown = dropdown;
+        dropdown.onValueChanged.AddListener(delegate
+        {
+            changer();
+        });
+       
+        
+        
+    }
+    public void changer()
+    {
+        setDirection(dropdown.captionText.text);
+    }
+
     override
     public void executer(int i)
     {
         Debug.Log(direction.ToString());
     }
-    public void setDirection(Direction direction)
+    public void setDirection(string direct)
     {
-        this.direction = direction;
+        if(direct == "Avant") 
+        {
+            direction = Direction.AVANT;
+        }
+        else if(direct == "Gauche")
+        {
+            direction = Direction.GAUCHE;
+        }else if(direct == "Droite")
+        {
+            direction = Direction.DROITE;
+        }
+        else
+        {
+            direction = Direction.ARRIERE;
+        }
     }
 }
