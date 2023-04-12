75899a51f59bba0f0486f396b67efbb9cce590c4
FAntoineLefebvre <123586434+FAntoineLefebvre@users.noreply.github.com>
2023-04-12 15:24:40 -0400

    Allo

--- projetSim/Assets/Scripts/ChangeScene.cs
+++ projetSim/Assets/Scripts/ChangeScene.cs
@@ -10,19 +10,24 @@ public class ChangeScene : MonoBehaviour
 
 
 
-    
+
     void Start()
     {
-        
+
     }
 
     public void changeScene()
     {
-        
+<<<<<<< HEAD
+        Ligne.viderList();
         SceneManager.LoadScene(sceneNameToLoad); // Change la scene
+=======
 
+        SceneManager.LoadScene(sceneNameToLoad); // Changer la scene
+
+>>>>>>> 06a99bc06be6bbe4b177e7eaa3356c8e32948b6b
     }
-    
-        
-    }
+
+
+}
 
